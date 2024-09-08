605dd083545d9662da1fd43595156e5997f6e0c6
DylanDHubert <dh6400a@student.american.edu>
2024-09-08 17:09:55 -0400

    updated to django

--- ThePantsProject/wsgi.py
+++ ThePantsProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoTemplate project.
+WSGI config for ThePantsProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoTemplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ThePantsProject.settings')
 
 application = get_wsgi_application()
